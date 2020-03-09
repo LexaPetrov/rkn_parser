bc8bb30e3092887d2eef7a4b2cba06bed245481c
aleksej <al1111997@yandex.ru>
2020-03-09 14:19:25 +0500

    Постраничный экспорт в Excel

--- program.py
+++ program.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup
 import requests as req
+import pandas as pd
+pd.io.formats.format.header_style = None
 
 user_agent = ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) '
               'Gecko/20100101 Firefox/50.0')
 
 url = 'http://rkn.gov.ru/communication/register/license/'
+
+# TODO: вывод всех 5 таблиц в 1 эксель файле
+#       при выводе форматирование ширины ячеек
+#       наименование лицензиата должно быть гиперссылкой, нужно вытаскивать href 
 
 # Вывести в файл весь документ
 # def save__to__file(resp, path):
@@ -26,49 +32,42 @@ def save__to__file(resp, path):
     else:
         f.close()
 
+# сохранить в excel
+def save__to__excel(resp, path):
+    soup = BeautifulSoup(resp.text, 'lxml')
+    soup = soup.find("table", id="ResList1")
+    print(soup)
+    t = pd.read_html(str(soup))
+    table = t[0].drop("Unnamed: 5", axis=1)
+    # table.to_excel(path, index=False)
 
-# Лишние вызовы функций потом удалю, пока посмотрите что да как работает (Алексей)
-# Главная страница
-save__to__file(
-    req.get(url, headers={'User-Agent':user_agent}),
-    'main_license_page.html'
-)
-
-
-# Тут страница где лежит ссылка "Отобразить результат"
-save__to__file(
-    req.post(
-        url, 
-        headers={'User-Agent':user_agent},
-        params={'SERVICE_ID': 12}
-    ),
-    'middle_2063_page.html'
-)
-
-
-# Тут первая страница 2063 итемов
-save__to__file(
-    req.post(
-        url + '?all=1',
-        headers={'User-Agent':user_agent},
-        params={'SERVICE_ID': 12}
-    ),
-    'items__page.html'
-)
+# Тут все нужные таблицы
+# for i in range (0, 5):
+#     save__to__file(
+#     req.post(
+#         url + 'p' + str(500 * i) + '/?all=1',
+#         headers={'User-Agent':user_agent},
+#         params={'SERVICE_ID': 12}
+#     ),
+#     'table__page' + str(i+1) + '.html'
+# )
 
 
-# Тут все нужные таблицы (Единственный нужный вызов)
-for i in range (0, 5, 1):
-    save__to__file(
-    req.post(
+# Тут 5 экселей
+for i in range (5):
+    save__to__excel(
+        req.post(
         url + 'p' + str(500 * i) + '/?all=1',
         headers={'User-Agent':user_agent},
         params={'SERVICE_ID': 12}
     ),
-    'table__page' + str(i+1) + '.html'
+    'table' + str(i + 1) + '.xlsx'
 )
+
 
 
 
 # https://python-scripts.com/beautifulsoup-html-parsing
 # https://www.softwaretestinghelp.com/selenium-webdriver-commands-selenium-tutorial-17/
+# https://stackoverflow.com/questions/35831241/converting-html-to-excel-in-python
+# https://www.geeksforgeeks.org/python-convert-an-html-table-into-excel/
